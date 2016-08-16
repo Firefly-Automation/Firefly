20557f478c85ec2305dba5a54f566973fe4235f4
Zachary Priddy <me@zpriddy.com>
2016-08-16 16:31:22 -0700

    try post not put

--- Firefly/devices/ffNest/ffNest.py
+++ Firefly/devices/ffNest/ffNest.py
@@ -2,7 +2,7 @@
 # @Author: Zachary Priddy
 # @Date:   2016-08-15 21:15:42
 # @Last Modified by:   Zachary Priddy
-# @Last Modified time: 2016-08-16 16:28:00
+# @Last Modified time: 2016-08-16 16:31:15
 
 import logging
 
@@ -157,12 +157,12 @@ class Device(Device):
 
   def setHome(self, args={}):
     logging.critical('SET HOME')
-    return self.setPresence(value='home')
+    return self.setPresence(value=True)
 
   def setAway(self, args={}):
-    return self.setPresence(value='away')
+    return self.setPresence(value=False)
 
-  def setPresence(self, value='home'):
+  def setPresence(self, value=True):
     logging.critical('SET NEST TO ' + str(value))
     
     presence = value
@@ -182,7 +182,7 @@ class Device(Device):
       'away' : presence
     }
 
-    r = requests.put(url, headers=headers, data=data)
+    r = requests.post(url, headers=headers, data=data)
 
     logging.critical(str(r.__dict__))
     
