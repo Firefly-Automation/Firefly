236a0becd6116a587d830f6e3467966a2911decc
Zachary Priddy <me@zpriddy.com>
2016-08-15 23:24:43 -0700

    blah

--- Firefly/devices/ffNest/ffNest.py
+++ Firefly/devices/ffNest/ffNest.py
@@ -2,7 +2,7 @@
 # @Author: Zachary Priddy
 # @Date:   2016-08-15 21:15:42
 # @Last Modified by:   Zachary Priddy
-# @Last Modified time: 2016-08-15 23:22:21
+# @Last Modified time: 2016-08-15 23:24:36
 
 import logging
 
@@ -78,6 +78,8 @@ class Device(Device):
     self._temp = None
     self._structure = None,
     self._structure_id = None
+
+    self.update()
 
 
     ###########################
