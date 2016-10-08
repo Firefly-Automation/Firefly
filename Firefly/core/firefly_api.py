7c89575f76e1ac6cf68062576a796a69447ca08f
Zachary Priddy <me@zpriddy.com>
2016-10-07 22:03:04 -0700

    debugging

--- Firefly/core/firefly_api.py
+++ Firefly/core/firefly_api.py
@@ -2,7 +2,7 @@
 # @Author: Zachary Priddy
 # @Date:   2016-04-11 08:56:32
 # @Last Modified by:   Zachary Priddy
-# @Last Modified time: 2016-10-07 22:01:35
+# @Last Modified time: 2016-10-07 22:02:55
 #
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not
@@ -445,6 +445,7 @@ def APIDevicesStatusAll(request):
   deviceTypeList = []
 
   for name, d in deviceViews.iteritems():
+    logging.critical(str(d))
     if d['type'] not in deviceTypeList:
       deviceTypeList.append(d['type'])
 
