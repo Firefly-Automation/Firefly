afe37b281219b3a06f88cb998d92b819a0edf786
Zachary Priddy <me@zpriddy.com>
2016-10-12 22:28:04 -0700

    blah

--- Firefly/run_firefly.py
+++ Firefly/run_firefly.py
@@ -3,7 +3,7 @@
 # @Author: Zachary Priddy
 # @Date:   2016-04-11 09:00:14
 # @Last Modified by:   Zachary Priddy
-# @Last Modified time: 2016-04-26 21:30:04
+# @Last Modified time: 2016-10-12 22:27:51
 #
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not
@@ -29,4 +29,6 @@ logging.getLogger("urllib3").setLevel(logging.WARNING)
 from core import firefly_api
 
 if __name__ == "__main__":
-  firefly_api.run()
+  #firefly_api.run()
+  from core import firefly
+  firefly.run()
