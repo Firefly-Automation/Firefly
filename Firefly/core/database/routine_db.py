3e243cfed5c7baaa5e07e85dfcd9af75a289a9bf
Zachary Priddy <me@zpriddy.com>
2016-10-13 02:58:57 -0700

    alexa fixes

--- Firefly/core/database/routine_db.py
+++ Firefly/core/database/routine_db.py
@@ -2,11 +2,11 @@
 # @Author: Zachary Priddy
 # @Date:   2016-10-12 22:09:46
 # @Last Modified by:   Zachary Priddy
-# @Last Modified time: 2016-10-12 22:12:55
+# @Last Modified time: 2016-10-13 02:58:43
 from core import routineDB
 
 def getRoutineList():
   routine_list = []
   for r in routineDB.find({}).sort('id'):
-    routine_list.append(dict(r))
+    routine_list.append(r.get('id'))
   return routine_list
