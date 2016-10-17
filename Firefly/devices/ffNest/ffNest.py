31925d9f92cf6627530cbdeb2db9368aede05b49
Zachary Priddy <me@zpriddy.com>
2016-10-17 00:08:23 -0700

    trying to fix nest

--- Firefly/devices/ffNest/ffNest.py
+++ Firefly/devices/ffNest/ffNest.py
@@ -45,10 +45,10 @@ class Device(Device):
       'type': 'thermostat',
       'card': "<md-card ><div layout='row'  layout-align='center center'><device-card layout='row' flex layout-wrap layout-align='center center'><span flex layout='row'><md-title layout-align='center center' style='cursor: pointer;' ng-click='selectDeviceIndex($index)'>{{ item.name }}</md-title></span><md-card-content layout-align='center center' style='padding:7px' layout='row'><md-button><ng-md-icon icon='expand_more' ng-click='setTarget(deviceStates[item.id].views.status.target-1.0)'></ng-md-icon></md-button><span layout-align='center center' style='font-size:20px'>{{deviceStates[item.id].views.status.current}}</span><md-button><ng-md-icon icon='expand_less' ng-click='setTarget(deviceStates[item.id].views.status.target+1.0)'></ng-md-icon></md-button></div></device-card><md-card-content ng-show='$index ==selectedDeviceIndex'><md-divider></md-divider><div layout='row' layout-align='center center' layout-wrap><md-button flex=50>On</md-button><md-button flex=50>Off</md-button></div><md-divider></md-divider><md-subhead> Turn off in: </md-subhead> <div layout='row' layout-align='center center'><md-button flex=25>30m</md-button><md-button flex=25>1h</md-button><md-button flex=25>2h</md-button><md-button flex=25>4h</md-button></div><br><md-card-actions layout='row' layout-align='start center'><md-button>More Info</md-button></md-card-actions></md-card-content></md-card-content></md-card>",
       'status': {
-        'current': self._temp,
-        'target': self._target_temperature,
-        'high': self._target_temperature_high,
-        'low': self._target_temperature_low
+        'current': 0,
+        'target': 0,
+        'high': 0,
+        'low': 0
       }
     }
 
