e36b5ca344737d41d240a8d5a67c59d135f8950d
Zachary Priddy <me@zpriddy.com>
2017-01-06 15:11:25 -0800

    Try new config for nest

--- Firefly/core/services/__init__.py
+++ Firefly/core/services/__init__.py
@@ -1,8 +1,14 @@
 from core import ffServices
 
 ffIndigo = None
+ffNest = None
 
 if ffServices.has_service('INDIGO'):
   if ffServices.get_boolean('INDIGO', 'enable'):
     from devices.ffIndigo.ffIndigoService import IndigoService
     ffIndigo = IndigoService()
+
+if ffServices.has_service('NEST'):
+  if ffServices.get_boolean('NEST', 'enable'):
+    import nest
+    ffNest = nest.Nest(ffServices.get_string('NEST','username'), ffServices.get_string('NEST','password'), local_time=True)
