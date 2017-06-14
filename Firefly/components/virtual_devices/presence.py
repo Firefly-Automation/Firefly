7d5a3cd1bd39ee2453fb505561cb08b6ce68674b
Zachary Priddy <github@zpriddy.com>
2017-06-14 11:29:55 -0700

    Fixed presence

--- Firefly/components/virtual_devices/presence.py
+++ Firefly/components/virtual_devices/presence.py
@@ -47,8 +47,9 @@ class VirtualPresence(Device):
         package:
         kwargs:
     """
-    kwargs['initial_values'] = INITIAL_VALUES if not kwargs.get('initial_values') else INITIAL_VALUES.update(
-      kwargs.get('initial_values'))
+    if kwargs.get('initial_values'):
+      INITIAL_VALUES.update(kwargs.get('initial_values'))
+    kwargs['initial_values'] = INITIAL_VALUES
     super().__init__(firefly, package, TITLE, AUTHOR, COMMANDS, REQUESTS, DEVICE_TYPE, **kwargs)
     self.__dict__.update(kwargs['initial_values'])
 
