aec0bff0a2dd970863b09c59607fb8c04e5a748d
Zachary Priddy <github@zpriddy.com>
2017-07-20 16:53:30 -0700

    Backups before auto-update

--- system_files/update_firefly.sh
+++ system_files/update_firefly.sh
@@ -1,5 +1,39 @@
 #!/usr/bin/env bash
 
+FIREFLY_ROOT="/opt/firefly_system"
+FIREFLY_META="/opt/firefly_system/.firefly"
+FIREFLY_BACKUP="/opt/firefly_system/.firefly/backup"
+FIREFLY_CONFIG="/opt/firefly_system/Firefly/dev_config"
+
+if [ ! -d "$FIREFLY_META" ]; then
+    mkdir $FIREFLY_META
+fi
+if [ ! -d "$FIREFLY_BACKUP" ]; then
+    mkdir $FIREFLY_BACKUP
+fi
+
+cd $FIREFLY_BACKUP
+if [ $(ls -1 | grep ^backup | wc -l) -eq 5 ]; then
+    rm -rf backup_5
+fi
+if [ $(ls -1 | grep ^backup | wc -l) -ge 4 ]; then
+    mv backup_4 backup_5
+fi
+if [ $(ls -1 | grep ^backup | wc -l) -ge 3 ]; then
+    mv backup_3 backup_4
+fi
+if [ $(ls -1 | grep ^backup | wc -l) -ge 2 ]; then
+    mv backup_2 backup_3
+fi
+if [ $(ls -1 | grep ^backup | wc -l) -ge 1 ]; then
+    mv backup_1 backup_2
+fi
+cp -r $FIREFLY_CONFIG backup_1
+cd backup_1
+touch ".$(date +%F_%R)"
+cd $FIREFLY_BACKUP
+
+
 cd /opt/firefly_system/Firefly
 git pull
 
--- system_files/update_scripts/update_0-0-1.sh
+++ system_files/update_scripts/update_0-0-1.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+FIREFLY_ROOT="/opt/firefly_system"
+FIREFLY_META="/opt/firefly_system/.firefly"
+FIREFLY_BACKUP="/opt/firefly_system/.firefly/backup"
+
 exit 0
