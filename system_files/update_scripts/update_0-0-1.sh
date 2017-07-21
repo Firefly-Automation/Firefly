0ea1d69c5bb6664865f34bb9107264079959dbe7
Zachary Priddy <github@zpriddy.com>
2017-07-20 17:03:53 -0700

    Print current version to file

--- system_files/update_scripts/update_0-0-1.sh
+++ system_files/update_scripts/update_0-0-1.sh
@@ -4,4 +4,4 @@ FIREFLY_ROOT="/opt/firefly_system"
 FIREFLY_META="/opt/firefly_system/.firefly"
 FIREFLY_BACKUP="/opt/firefly_system/.firefly/backup"
 
-exit 0
+echo "0.0.0.a" > $FIREFLY_META/current_version
