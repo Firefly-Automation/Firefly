bc16403c95c3bc048af03e53094917ac983cf71a
Zachary Priddy <github@zpriddy.com>
2017-07-21 09:21:30 -0700

    Added support for Aeotc Power Strip
    Added support for switching each outlet individually
    
    Still lacking support for power meter

--- Firefly/components/zwave/zwave_aeotec_dsc11_smart_strip.py
+++ Firefly/components/zwave/zwave_aeotec_dsc11_smart_strip.py
@@ -2,18 +2,18 @@ from openzwave.network import ZWaveNode
 
 from Firefly import logging
 from Firefly.components.zwave.zwave_device import ZwaveDevice
-from Firefly.const import (ACTION_OFF, ACTION_ON, ACTION_TOGGLE, DEVICE_TYPE_SWITCH, EVENT_ACTION_OFF, EVENT_ACTION_ON,
-                           STATE, ALEXA_ON, ALEXA_OFF)
+from Firefly.const import ACTION_OFF, ACTION_ON, ACTION_TOGGLE, ALEXA_OFF, ALEXA_ON, DEVICE_TYPE_SWITCH, EVENT_ACTION_OFF, EVENT_ACTION_ON, STATE
 from Firefly.helpers.metadata import metaSwitch
 
 TITLE = 'Firefly Aeotec SDSC11 Smart Strip'
 DEVICE_TYPE = DEVICE_TYPE_SWITCH
 AUTHOR = 'Zachary Priddy'
-COMMANDS = [ACTION_OFF, ACTION_ON, ACTION_TOGGLE]
-REQUESTS = [STATE]
+COMMANDS = [ACTION_OFF, ACTION_ON, ACTION_TOGGLE, 'switch1', 'switch2', 'switch3', 'switch4', 'switchoff1', 'switchoff2', 'switchoff3', 'switchoff4']
+REQUESTS = [STATE, 'switch1', 'switch2', 'switch3', 'switch4']
 INITIAL_VALUES = {
   '_state': EVENT_ACTION_OFF
 }
+
 
 # https://aeotec.freshdesk.com/helpdesk/attachments/6009584527
 
@@ -39,13 +39,34 @@ class Dsc11(ZwaveDevice):
     self.add_command(ACTION_OFF, self.off)
     self.add_command(ACTION_ON, self.on)
     self.add_command(ACTION_TOGGLE, self.toggle)
+    self.add_command('switch1', self.switch1)
+    self.add_command('switch2', self.switch2)
+    self.add_command('switch3', self.switch3)
+    self.add_command('switch4', self.switch4)
+    self.add_command('switch1', self.switchoff1)
+    self.add_command('switch2', self.switchoff2)
+    self.add_command('switch3', self.switchoff3)
+    self.add_command('switch4', self.switchoff4)
 
     self.add_request(STATE, self.get_state)
+    self.add_request('switch1', self.get_state1)
+    self.add_request('switch2', self.get_state2)
+    self.add_request('switch3', self.get_state3)
+    self.add_request('switch4', self.get_state4)
 
     self.add_action(STATE, metaSwitch())
+    self.add_action('switch1', metaSwitch(on_action='switch1', off_action='switchoff1', title='Outlet 1', control_type='switch'))
+    self.add_action('switch2', metaSwitch(on_action='switch2', off_action='switchoff2', title='Outlet 2', control_type='switch'))
+    self.add_action('switch3', metaSwitch(on_action='switch3', off_action='switchoff3', title='Outlet 3', control_type='switch'))
+    self.add_action('switch4', metaSwitch(on_action='switch4', off_action='switchoff4', title='Outlet 4', control_type='switch'))
 
     self.add_alexa_action(ALEXA_OFF)
     self.add_alexa_action(ALEXA_ON)
+
+    self._state1 = EVENT_ACTION_OFF
+    self._state2 = EVENT_ACTION_OFF
+    self._state3 = EVENT_ACTION_OFF
+    self._state4 = EVENT_ACTION_OFF
 
   def update_device_config(self, **kwargs):
     # TODO: Pull these out into config values
@@ -92,22 +113,53 @@ class Dsc11(ZwaveDevice):
     if self._switches is None:
       self._switches = list(self._node.get_switches().keys())
 
-    if node.get_switch_state(self._switches[0]):
-      self._state = EVENT_ACTION_ON
-    else:
-      self._state = EVENT_ACTION_OFF
+    self._state = EVENT_ACTION_ON if node.get_switch_state(self._switches[0]) else EVENT_ACTION_OFF
+    self._state1 = EVENT_ACTION_ON if node.get_switch_state(self._switches[1]) else EVENT_ACTION_OFF
+    self._state2 = EVENT_ACTION_ON if node.get_switch_state(self._switches[2]) else EVENT_ACTION_OFF
+    self._state3 = EVENT_ACTION_ON if node.get_switch_state(self._switches[3]) else EVENT_ACTION_OFF
+    self._state4 = EVENT_ACTION_ON if node.get_switch_state(self._switches[4]) else EVENT_ACTION_OFF
 
   def off(self, **kwargs):
     self._state = EVENT_ACTION_OFF
     self.setOff()
     return EVENT_ACTION_OFF
 
+  def switchoff1(self, **kwargs):
+    self.member_set('_state1', EVENT_ACTION_OFF)
+    self.setOff(1)
+
+  def switchoff2(self, **kwargs):
+    self.member_set('_state2', EVENT_ACTION_OFF)
+    self.setOff(2)
+
+  def switchoff3(self, **kwargs):
+    self.member_set('_state3', EVENT_ACTION_OFF)
+    self.setOff(3)
+
+  def switchoff4(self, **kwargs):
+    self.member_set('_state4', EVENT_ACTION_OFF)
+    self.setOff(4)
+
   def on(self, **kwargs):
     self._state = EVENT_ACTION_ON
     self.setOn()
     return EVENT_ACTION_ON
 
+  def switch1(self, **kwargs):
+    self.member_set('_state1', EVENT_ACTION_ON)
+    self.setOn(1)
 
+  def switch2(self, **kwargs):
+    self.member_set('_state2', EVENT_ACTION_ON)
+    self.setOn(2)
+
+  def switch3(self, **kwargs):
+    self.member_set('_state3', EVENT_ACTION_ON)
+    self.setOn(3)
+
+  def switch4(self, **kwargs):
+    self.member_set('_state4', EVENT_ACTION_ON)
+    self.setOn(4)
 
   def setOn(self, switch=0):
     self._node.set_switch(self._switches[switch], 1)
@@ -122,6 +174,18 @@ class Dsc11(ZwaveDevice):
 
   def get_state(self, **kwargs):
     return self.state
+
+  def get_state1(self, **kwargs):
+    return self._state1
+
+  def get_state2(self, **kwargs):
+    return self._state2
+
+  def get_state3(self, **kwargs):
+    return self._state3
+
+  def get_state4(self, **kwargs):
+    return self._state4
 
   @property
   def state(self):
