d90fa18480b49b907e0cad501570be75335f1f1e
Zachary Priddy <github@zpriddy.com>
2017-09-15 23:56:48 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -20,7 +20,7 @@ echo ""
 mkdir $FIREFLY_ROOT
 mkdir $FIREFLY_ROOT/python3.6
 cd $FIREFLY_ROOT/python3.6
-apt-get install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-devibc6-dev
+apt-get install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libc6-dev
 wget https://www.python.org/ftp/python/3.6.2/Python-3.6.2.tgz
 tar xzvf Python-3.6.2.tgz
 cd Python-3.6.2/
