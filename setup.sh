5974b662a3de7decded09b3eeba4810ebc53a015
Zachary Priddy <github@zpriddy.com>
2017-09-14 20:04:17 -0700

    Update setup

--- setup.sh
+++ setup.sh
@@ -9,60 +9,78 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-sudo apt-get dist-upgrade -y
+apt-get dist-upgrade -y
+
+echo ""
+echo "******************************************************"
+echo " INSTALLING PYTHON 3.6"
+echo "******************************************************"
+echo ""
 
 mkdir $FIREFLY_ROOT
 mkdir $FIREFLY_ROOT/python3.6
 cd $FIREFLY_ROOT/python3.6
-sudo apt-get install -y build-essential checkinstall
-sudo apt-get install -y libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
+apt-get install -y build-essential checkinstall
+apt-get install -y libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
 wget https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tgz
 tar xzvf Python-3.6.1.tgz
 cd Python-3.6.1/
 ./configure
-sudo make install
+make install
 
-sudo rm /usr/bin/pip3
-sudo rm /usr/bin/python3-config
-sudo rm /usr/bin/python3
+rm /usr/bin/pip3
+rm /usr/bin/python3-config
+rm /usr/bin/python3
 
-sudo pip3 install docutils pygments roman
+echo ""
+echo "******************************************************"
+echo " INSTALLING PYTHON OPEN ZWAVE"
+echo "******************************************************"
+echo ""
+
+pip3 install docutils pygments roman
 cd $FIREFLY_ROOT
 git clone https://github.com/OpenZWave/python-openzwave.git
 cd python-openzwave/
-sudo make common-deps PYTHON_EXEC=python3
+make common-deps PYTHON_EXEC=python3
 make build PYTHON_EXEC=python3
-sudo pip3 install cython
-sudo make install PYTHON_EXEC=python3
+pip3 install cython
+make install PYTHON_EXEC=python3
 
+echo ""
+echo "******************************************************"
+echo " INSTALLING PFIREFLY"
+echo "******************************************************"
+echo ""
 
 cd $FIREFLY_ROOT
 git clone https://github.com/zpriddy/forecastiopy.git
 cd forecastiopy/
-sudo python3 setup.py install
+python3 setup.py install
 
-sudo pip3 install pytz
+pip3 install pytz
 cd $FIREFLY_ROOT
 git clone https://github.com/zpriddy/astral.git
 cd astral/
-sudo python3 setup.py install
+python3 setup.py install
 
 cd $FIREFLY_ROOT
 git clone https://github.com/zpriddy/Pyrebase.git
 cd Pyrebase
-sudo pip3 install -r requirements.txt
-sudo python3 setup.py install
+pip3 install -r requirements.txt
+python3 setup.py install
 
 cd $FIREFLY_ROOT
 git clone https://github.com/Firefly-Automation/Firefly.git
 cd Firefly
-sudo pip3 install -r requirements.txt
+pip3 install -r requirements.txt
 cp -r sample_config dev_config
 echo [] > dev_config/automation.json
 
 #####################################
 # SETUP BEACON
 #####################################
+mkdir $FIREFLY_META
 BEACON_ID=$(cat /dev/urandom | tr -dc '0-9A-F' | fold -w 32 | head -n 1 | sed -e 's/\(..\)/\1 /g')
 echo \beacon=$BEACON_ID >> $FIREFLY_ROOT/Firefly/dev_config/firefly.config
 echo $BEACON_ID > $FIREFLY_META/beacon_id
@@ -76,4 +94,4 @@ echo "0.0.0.b" > $FIREFLY_META/current_version
 # SETUP AUTO-START
 #####################################
 cd $FIREFLY_ROOT/Firefly/system_files
-sudo bash setup_autostart.sh
+bash setup_autostart.sh
