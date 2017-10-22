30ec3ce4a32fa58ec78159677de832fd8398a5de
Zachary Priddy <github@zpriddy.com>
2017-10-22 08:07:21 -0700

    More fixes

--- Firefly/services/firebase.py
+++ Firefly/services/firebase.py
@@ -375,6 +375,10 @@ class Firebase(Service):
     for device in devices:
       device_views[device.get('ff_id', 'unknown')] = device
     self.set_home_status(FIREBASE_DEVICE_VIEWS, device_views)
+
+    #TODO: Remove this
+    self.set_home_status('devices', device_views)
+
     self.update_aliases()
     self.update_last_metadata_timestamp()
 
@@ -399,7 +403,7 @@ class Firebase(Service):
         pass
 
     #TODO Remove this
-    self.set_home_status('devices', all_values)
+    self.update_home_status('devices', all_values)
 
     if overwrite:
       self.set_home_status(FIREBASE_DEVICE_STATUS, all_values)
