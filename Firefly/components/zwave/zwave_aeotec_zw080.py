bcc413971b69e0f384c40289048d4f0a7adaba69
Zachary Priddy <github@zpriddy.com>
2017-11-17 14:38:21 -0700

    Alarm updates

--- Firefly/components/zwave/zwave_aeotec_zw080.py
+++ Firefly/components/zwave/zwave_aeotec_zw080.py
@@ -4,7 +4,7 @@ from Firefly import logging
 from Firefly.components.zwave.zwave_device import ZwaveDevice
 from Firefly.const import (ACTION_OFF, ACTION_ON, STATE, EVENT_ACTION_OFF, EVENT_ACTION_ON,
                            ACTION_TOGGLE, DEVICE_TYPE_SWITCH, SWITCH, ALEXA_ON, ALEXA_OFF)
-from Firefly.helpers.metadata import metaSwitch, metaText, action_text, action_on_off_switch
+from Firefly.helpers.metadata import metaSwitch, metaText, action_text, action_on_off_switch, ColorMap
 
 TITLE = 'Aeotec Alarm ZW080'
 DEVICE_TYPE = DEVICE_TYPE_SWITCH
@@ -51,8 +51,9 @@ class ZW080(ZwaveDevice):
     self.add_request('alarm3', self.get_state)
     self.add_request('alarm4', self.get_state)
     self.add_request('alarm5', self.get_state)
-
-    self.add_action(STATE, action_text(False, True, True, 'Alarm', 'Alarm is going off', SWITCH, 'off', text_mapping={'red':['on'], 'green':['off']}))
+    
+    cm = ColorMap(green=['off'], red=['on'])
+    self.add_action(STATE, action_text(False, True, True, 'Alarm', 'Alarm is going off', SWITCH, 'off', text_mapping={'Alarm':['on'], 'No Alarm':['off']}, color_mapping=cm))
     #self.add_action('alarm1', metaSwitch(on_action='alarm1', title='Alarm 1', control_type='switch'))
     #self.add_action('alarm2', metaSwitch(on_action='alarm2', title='Alarm 2', control_type='switch'))
     #self.add_action('alarm3', metaSwitch(on_action='alarm3', title='Alarm 3', control_type='switch'))
